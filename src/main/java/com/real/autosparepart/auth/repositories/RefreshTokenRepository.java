af8c11e9ce98dc02721ee62d28222d89d3ec99eb
yanpich <yanpich154@gmail.com>
2026-05-01 23:54:24 +0700

    Update README.md and auth

--- src/main/java/com/real/autosparepart/auth/repositories/RefreshTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.real.autosparepart.auth.repositories;
-
-import com.real.autosparepart.auth.entity.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-}
